abab3c0ffef6547d7907802cf82afcdcff8a103c
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-15 18:59:06 -0700

    add

---
+++ 100 and 200 which are divisible by 9/main.js
@@ -0,0 +1,23 @@
+
+
+	function key(){
+		
+		sum=0;
+		
+		
+		
+		for(i=100;i<=200;i++){
+			
+			if(i%9==0){
+			
+			sum+=i;
+			console.log(i);
+			}
+			
+			
+		}
+		console.log(sum);
+		
+		
+	}
+	key();
